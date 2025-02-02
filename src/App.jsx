9084de49a095eac061ab7a1a504e52100dea2a6c
learner-of-string <aref0171personal@gmail.com>
2025-02-02 12:12:48 +0600

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-'
-
 function App() {
-  
-
   return (
-   <div>
-    
-   </div>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold bg-green-200 p-3 border-green-600 border-2 rounded-2xl text-green-500">
+          We are here to learn react from Tapascript.
+        </h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
